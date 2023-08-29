261d42378d12705a27190dce111eaf893737c900
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2023-08-29 09:55:32 +0300

    Add files via upload
    Spanish lang. added

---
+++ IMSProg_editor/language/chipEditor_es_ES.ts
@@ -0,0 +1,271 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="14"/>
+        <source>IMSProg chip database editor</source>
+        <translation>Editor de bases de datos de chips IMSProg</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="49"/>
+        <source>File</source>
+        <translation>Archivo</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="60"/>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="98"/>
+        <source>Open</source>
+        <translation>Abrir</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="101"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="110"/>
+        <source>Exit</source>
+        <translation>Salida</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="113"/>
+        <location filename="../ezp_chip_editor.ui" line="206"/>
+        <source>Ctrl+X</source>
+        <translation>Ctrl+X</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="122"/>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="125"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="134"/>
+        <source>Delete lines</source>
+        <translation>Borrar líneas</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="137"/>
+        <source>Delete selected lines</source>
+        <translation>Borrar las líneas seleccionadas</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="140"/>
+        <source>Del</source>
+        <translation>Del</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="149"/>
+        <source>Add line</source>
+        <translation>Añadir línea</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="152"/>
+        <source>Add selected lines and duplicate data</source>
+        <translation>Añadir líneas seleccionadas y duplicar datos</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="155"/>
+        <source>Ins</source>
+        <translation>Ins</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="164"/>
+        <source>Move up</source>
+        <translation>Sube</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="167"/>
+        <source>Move lines up</source>
+        <translation>Subir líneas</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="170"/>
+        <source>Ctrl+Up</source>
+        <translation>Ctrl+Up</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="179"/>
+        <source>Move down</source>
+        <translation>Baja</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="182"/>
+        <source>Move lines down</source>
+        <translation>Bajar líneas</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="185"/>
+        <source>Ctrl+Down</source>
+        <translation>Ctrl+Down</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="194"/>
+        <source>Export selected lines to CSV file</source>
+        <translation>Exportar las líneas seleccionadas a un archivo CSV</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="203"/>
+        <source>Export to CSV</source>
+        <translation>Exportar a CSV</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="215"/>
+        <source>Import from CSV</source>
+        <translation>Importar desde CSV</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="218"/>
+        <source>, Ctrl+Shift+X</source>
+        <translation>, Ctrl+Shift+X</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="44"/>
+        <location filename="../ezp_chip_editor.cpp" line="46"/>
+        <location filename="../ezp_chip_editor.cpp" line="722"/>
+        <location filename="../ezp_chip_editor.cpp" line="743"/>
+        <source>Open the file</source>
+        <translation>Abrir el expediente</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="49"/>
+        <location filename="../ezp_chip_editor.cpp" line="687"/>
+        <location filename="../ezp_chip_editor.cpp" line="725"/>
+        <location filename="../ezp_chip_editor.cpp" line="746"/>
+        <source>Current file: </source>
+        <translation>Archivo actual:</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="73"/>
+        <source>Type</source>
+        <translation>Tipo</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="74"/>
+        <source>Manufacture</source>
+        <translation>Fabricación</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="75"/>
+        <source>IC Name</source>
+        <translation>Nombre IC</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="76"/>
+        <source>JEDEC ID</source>
+        <translation>JEDEC ID</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="77"/>
+        <source>Size</source>
+        <translation>Talla</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="78"/>
+        <source>Sector
+size</source>
+        <translation>Sector
+tamaño</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="79"/>
+        <source>Type
+HEX</source>
+        <translation>Tipo
+HEX</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="80"/>
+        <source>Algo-
+rithm</source>
+        <translation>Algo-
+ritmo</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="81"/>
+        <source>Delay</source>
+        <translation>Retraso</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="82"/>
+        <source>4 bit
+address</source>
+        <translation>4 bits
+dirección</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="83"/>
+        <source>Block
+size K</source>
+        <translation>Bloque
+tamaño K</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="84"/>
+        <source>EEPROM
+pages</source>
+        <translation>EEPROM
+páginas</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="85"/>
+        <source>VCC</source>
+        <translation>Питание</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="518"/>
+        <location filename="../ezp_chip_editor.cpp" line="555"/>
+        <location filename="../ezp_chip_editor.cpp" line="583"/>
+        <location filename="../ezp_chip_editor.cpp" line="622"/>
+        <location filename="../ezp_chip_editor.cpp" line="662"/>
+        <location filename="../ezp_chip_editor.cpp" line="782"/>
+        <location filename="../ezp_chip_editor.cpp" line="814"/>
+        <source>Warning</source>
+        <translation>Advertencia</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="518"/>
+        <location filename="../ezp_chip_editor.cpp" line="555"/>
+        <location filename="../ezp_chip_editor.cpp" line="583"/>
+        <location filename="../ezp_chip_editor.cpp" line="622"/>
+        <location filename="../ezp_chip_editor.cpp" line="662"/>
+        <source>No string selected.</source>
+        <translation>No se ha seleccionado ninguna cadena.</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="682"/>
+        <source>Saving file</source>
+        <translation>Guardar archivo</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="684"/>
+        <source>Save file</source>
+        <translation>Guardar archivo</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="741"/>
+        <source>Opening file</source>
+        <translation>Abrir expediente</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="782"/>
+        <source>Invalid CSV data file format.</source>
+        <translation>Formato de archivo de datos CSV no válido.</translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="814"/>
+        <source>Invalid CSV header file format.</source>
+        <translation>Formato de archivo de cabecera CSV no válido.</translation>
+    </message>
+</context>
+</TS>
