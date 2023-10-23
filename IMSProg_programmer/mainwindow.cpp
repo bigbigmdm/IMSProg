70e0cfc9421c594b01f0ccb85b8e7bc1023bae1a
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2023-10-23 16:43:53 +0300

    Update mainwindow.cpp
    Bug deleted: editor not working

--- IMSProg_programmer/mainwindow.cpp
+++ IMSProg_programmer/mainwindow.cpp
@@ -1227,7 +1227,7 @@ void MainWindow::on_actionEdit_chips_Database_triggered()
 {
     if(QFileInfo::exists("IMSProg_editor") && !QDir("IMSProg_editor").exists()){
         //The file exists and is not a folder
-        QProcess::execute("IMSProg_editor");
+        QProcess::execute("./IMSProg_editor");
     }
     else {
         //The file doesn't exist, either the path doesn't exist or is the path of a folder
