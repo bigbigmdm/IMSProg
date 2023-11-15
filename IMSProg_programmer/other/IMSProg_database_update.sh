4a024bcce6c9f759410131a06fa14a55574de016
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2023-11-15 08:09:45 +0300

    Add files via upload
    manual merging

--- IMSProg_programmer/other/IMSProg_database_update.sh
+++ IMSProg_programmer/other/IMSProg_database_update.sh
@@ -2,5 +2,7 @@
 PASSWORD=$(zenity --password)
 cd /tmp
 wget https://antenna-dvb-t2.ru/dl_all/IMSProg.Dat
-echo $PASSWORD | sudo -S cp IMSProg.Dat /opt/IMSProg
+cd /etc
+mkdir -p imsprog
+echo $PASSWORD | sudo -S cp /tmp/IMSProg.Dat /etc/imsprog/
 rm -rf /tmp/IMSProg.Dat
