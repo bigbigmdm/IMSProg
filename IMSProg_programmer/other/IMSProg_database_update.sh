57ee98a4f1ff8cf1f915933ed516f039508dfe33
Mikhail Medvedev <e-ink-reader@yandex.ru>
2023-11-22 10:08:03 +0300

    Added new function in the update script

--- IMSProg_programmer/other/IMSProg_database_update.sh
+++ IMSProg_programmer/other/IMSProg_database_update.sh
@@ -4,5 +4,16 @@ cd /tmp
 wget https://antenna-dvb-t2.ru/dl_all/IMSProg.Dat
 cd /etc
 mkdir -p imsprog
+file=/etc/imsprog/IMSProg.Dat
+oldsize=$(wc -c <"$file")
+let oldrec=oldsize/68-1
 echo $PASSWORD | sudo -S cp /tmp/IMSProg.Dat /etc/imsprog/
 rm -rf /tmp/IMSProg.Dat
+newsize=$(wc -c <"$file")
+let newrec=newsize/68-1
+out_text=$(echo $oldrec "->" $newrec)
+echo $oldrec "->" $newrec
+zenity --warning \
+--text="The number of chips in the database:\n\n$out_text" \
+--title="Database update" \
+--icon-name='applications-electronics'
