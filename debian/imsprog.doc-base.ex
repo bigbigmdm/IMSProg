77e5357a00df172865174a6d176ebfa2f1b10754
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2023-12-01 14:00:19 +0300

    Delete debian/imsprog.doc-base.ex

--- debian/imsprog.doc-base.ex
+++
@@ -1,9 +0,0 @@
-Document: imsprog
-Title: Debian imsprog Manual
-Author: Mikhail Medvedev
-Abstract: This manual describes what imsprog on Debian systems.
-Section: Programming
-
-Format: HTML
-Index: /usr/share/doc/imsprog/html/index.html
-Files: /usr/share/doc/imsprog/html/*.html
