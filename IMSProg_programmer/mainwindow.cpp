4eecbaf617944e70cf1540e2c96e882f0df66cb6
Mikhail Medvedev <e-ink-reader@yandex.ru>
2024-01-26 15:47:46 +0300

    Added copyright and license information

--- IMSProg_programmer/mainwindow.cpp
+++ IMSProg_programmer/mainwindow.cpp
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 2023 Mikhail Medvedev <e-ink-reader@yandex.ru>
+ * Copyright (C) 2023 - 2024 Mikhail Medvedev <e-ink-reader@yandex.ru>
  *
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
+ * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
