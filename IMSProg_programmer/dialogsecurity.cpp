fc84b2f61552b07ad893d6a94056877689b988b7
Mikhail Medvedev <e-ink-reader@yandex.ru>
2025-02-25 13:14:35 +0300

    Added MXIC OTP algorithm

--- IMSProg_programmer/dialogsecurity.cpp
+++ IMSProg_programmer/dialogsecurity.cpp
@@ -41,7 +41,7 @@ void DialogSecurity::setAlgorithm(uint8_t currentAlg)
       { 1,  0x48,  0x42, 0x44,    0,     0,    1},  //Winbond, Gigadevice, Boya, UCUNDATA, Fudan, Zetta, Zbit, XMC
       { 2,  0x03,  0x02, 0x20, 0x3a,  0x04,    0},  //EON
       { 3,  0x68,  0x62, 0x64,    0,     0,    1},  //ISSI
-      { 4,  0x03,  0x02, 0x20, 0xb1,  0xc1,    0},  //MXIC, Fidelix, Zetta
+      { 4,  0x03,  0x02, 0x00, 0xb1,  0xc1,    0},  //MXIC, Fidelix, Zetta
     };
     algSettings algSet[] = {
     //    id algType RegNum Size   rg0add   rg1add   rg2add   rg3add allErase a4byte curCommand
@@ -67,10 +67,15 @@ void DialogSecurity::setAlgorithm(uint8_t currentAlg)
       { 0x13,      0,     3,   4,  0x0040,  0x0080,  0x00c0,       0,       0,     0,         1}, //CXF
       { 0x14,      0,     2,  16,  0x0010,  0x0020,       0,       0,       0,     0,         1}, //XTX
       { 0x15,      0,     2,   4,  0x0000,  0x0001,       0,       0,       0,     0,         1}, //XTX
+      { 0x16,      1,     1,   8,  0x0000,       0,       0,       0,       0,     0,         4}, //MXIC
+      { 0x17,      1,     1,   1,  0x0000,       0,       0,       0,       0,     0,         4}, //MXIC
+      { 0x18,      1,     2,   8,  0x0000,  0x0020,       0,       0,       0,     0,         4}, //MXIC
     };
     curAlg = currentAlg;
     curSettings = algSet[curAlg];
     curCommands = comPattern[curSettings.curCommand];
+    if (curCommands.ERSCUR == 0) ui->toolButton_erase->setDisabled(true);
+    else ui->toolButton_erase->setDisabled(false);
 
     if (curAlg > 0)
     {
@@ -298,8 +303,6 @@ void DialogSecurity::on_toolButton_erase_clicked()
        a23a16 = static_cast<uint8_t>(curRgAddr >> 8);
        a15a08 = static_cast<uint8_t>(curRgAddr & 0x00ff);
 
-       if (curSettings.algType == 0)
-       {
            if (curSettings.algType == 1)
            {
            //Enter OTP mode
@@ -346,10 +349,10 @@ void DialogSecurity::on_toolButton_erase_clicked()
            SPI_CONTROLLER_Write_One_Byte(0x04);  // Write Disable
            SPI_CONTROLLER_Chip_Select_High();
            usleep(1);
-       }
+    }
        else QMessageBox::about(this, tr("Error"), tr("Programmer CH341a is not connected!"));
        ch341a_spi_shutdown();
-   }
+
 }
 
 void DialogSecurity::setPath(QString lastPath)
