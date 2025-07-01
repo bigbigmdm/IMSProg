88f934d435d9157d4d2dbd17bb232d0bf7564a32
Mikhail Medvedev <e-ink-reader@yandex.ru>
2025-07-01 10:41:33 +0300

    Preparing a new version

--- IMSProg_programmer/searchdialog.cpp
+++ IMSProg_programmer/searchdialog.cpp
@@ -2,15 +2,19 @@
  * Copyright (C) 2015-2016 Winfried Simon
  * Copyright (C) 2023 - 2024 Mikhail Medvedev <e-ink-reader@yandex.ru>
  *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This software may be used under the terms of the GNU Lesser General 
- * Public License version 2.1 as published by the Free Software Foundation 
- * and appearing in the file license.txt included in the packaging of this file.
- *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * https://www.gnu.org/licenses/
  */
 #include "searchdialog.h"
 #include "ui_searchdialog.h"
