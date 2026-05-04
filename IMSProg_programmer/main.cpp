64e56b044e1b68b50e61eb4d909bd8baea9d177c
Andrei Cherniaev <27889022+AndreiCherniaev@users.noreply.github.com>
2026-05-05 02:22:57 +0900

    Del deprecated
    warning: ‘Qt::AA_EnableHighDpiScaling’ is deprecated: High-DPI scaling is always enabled. This attribute no longer has any effect.

--- IMSProg_programmer/main.cpp
+++ IMSProg_programmer/main.cpp
@@ -76,7 +76,6 @@ static void initPaths()
 
 int main(int argc, char *argv[])
 {
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication::setApplicationName("imsprog");
     QFont font("Monospace");
     font.setStyleHint(QFont::TypeWriter);
